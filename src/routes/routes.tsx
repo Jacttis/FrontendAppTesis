ca9430f00c5ecbf06a20b1beaa227cdd5985232b
LeanM <42581931+LeanM@users.noreply.github.com>
2023-10-05 17:54:24 -0300

    Update routes.tsx

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,52 +1,40 @@
-import Welcome from '../pages/Welcome/welcome'
-import Login from '../pages/Login/login'
-import Test from '../pages/Welcome/Test'
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import VectorIcon from 'react-native-vector-icons/Ionicons';
-import Home from '../pages/Home/home';
-
+import Welcome from "../pages/Welcome/welcome";
+import Login from "../pages/Login/login";
+import Test from "../pages/Welcome/Test";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import VectorIcon from "react-native-vector-icons/Ionicons";
+import Home from "../pages/Home/home";
 
 const Tab = createBottomTabNavigator();
-const RootStack = createNativeStackNavigator()
+const RootStack = createNativeStackNavigator();
 
 //TODO: Lean aca agregarias las cosas, Fijate y borra los comentarios despues.
 //      El login no que lo estoy modificando despues rompe tod0
 export const BottomTabs = () => (
-    <Tab.Navigator>
-        <Tab.Screen
-            name="Welcome"
-            component={Welcome}
-            options={{
-                tabBarIcon: ({focused}) => (
-                    <VectorIcon name="home" color='black' size={30}/>
-                ),
-            }}
-        />
-        <Tab.Screen
-            name="Test"
-            component={Test}
-
-        />
-    </Tab.Navigator>
-);
-
-export const HomeTabs = () => (
-    <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home}/>
-    </Tab.Navigator>
+  <Tab.Navigator>
+    <Tab.Screen
+      name="home"
+      component={Home}
+      options={{
+        tabBarIcon: ({ focused }) => (
+          <VectorIcon name="home" color="black" size={30} />
+        ),
+      }}
+    />
+  </Tab.Navigator>
 );
 
 export const MainStack = () => (
-    <RootStack.Navigator initialRouteName={"home"}>
-        <RootStack.Screen name={"login"} component={Login}/>{/*//esto es solo una screen */}
-        <RootStack.Screen name={"bottomTabs"} component={BottomTabs}/>{/*// en cambio esto es un stack*/}
-        {/*aca si vos queres agregar mas screens o stacks*/}
-        <RootStack.Screen name={"home"} component={Home} />
-    </RootStack.Navigator>
-)
+  <RootStack.Navigator initialRouteName={"login"}>
+    <RootStack.Screen name={"login"} component={Login} />
+    {/*//esto es solo una screen */}
+    <RootStack.Screen name={"bottomTabs"} component={BottomTabs} />
+    {/*// en cambio esto es un stack*/}
+    {/*aca si vos queres agregar mas screens o stacks*/}
+  </RootStack.Navigator>
+);
 //aca devolvemos el stack principal. el que tiene tod0
 export default function Routes() {
-    return <MainStack/>
+  return <MainStack />;
 }
-
