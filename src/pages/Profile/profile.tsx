b75faa497d03bb728e7f708d2d7f5025ebd7fb8a
jacttis <julianacttis@hotmail.com>
2023-11-16 17:54:20 -0300

    refactor profile

--- src/pages/Profile/profile.tsx
+++ src/pages/Profile/profile.tsx
@@ -1,12 +1,12 @@
-import React, { useContext, useEffect, useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image, SafeAreaView, TextInput, Linking, Alert } from "react-native";
+import React, { useState, useContext, useEffect } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput, SafeAreaView, ScrollView } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import { AuthContext } from "../../context/AuthContext";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { AddressSuggestion, getLocation } from "../../services/GeoLocationService";
-import { getWorker } from "../../connection/requests";
+import { getClient, getWorker } from "../../connection/requests";
 
-export default function Profile() {
-  const { userToken, signOut, email } = useContext(AuthContext);
+const ProfileScreen = () => {
+  const { role,userToken, signOut, email } = useContext(AuthContext);
+
 
   type UserProps = {
     email: string;
@@ -15,195 +15,206 @@ export default function Profile() {
     picture: string;
     description: string;
     phoneNumber: string;
-    birthDate: string;
-    profession: string;
+    birthDate: Date;
+    profession: {
+      professionName: string;
+    };
     certificate: string;
   };
 
   useEffect(() => {
-    console.log(userToken);
-    console.log(email);
-    getWorker(userToken, email).then((response) => {
-        setProfile(response.data);
-      }
-      ).catch( (error) => console.log(error)
-    );
+    if(role == "worker") {
+      getWorker(userToken, email).then((response) => {
+          setProfile(response.data);
+        }
+      ).catch((error) => console.log(error)
+      );
+    }
+    else {
+      getClient(userToken, email).then((response) => {
+          setProfile(response.data);
+        }
+      ).catch((error) => console.log(error)
+      );
+    }
+
 
   }, []);
 
+  // Estado inicial del perfil
   const [profile, setProfile] = useState<UserProps>({
-    address: "",
-    birthDate: "",
-    certificate: "",
-    description: "",
-    email: "",
     name: "",
+    email: "",
     phoneNumber: "",
     picture: "https://via.placeholder.com/150",
-    profession: ""
-
+    address: "",
+    description: "",
+    profession: { professionName: "" },
+    certificate: "",
+    birthDate: new Date()
   });
 
+  // Gestionar los campos de edición
   const [editingField, setEditingField] = useState(null);
   const [tempValue, setTempValue] = useState("");
 
-  const handleEdit = (field: React.SetStateAction<null>, value: React.SetStateAction<string>) => {
+  // Función para iniciar la edición de un campo
+  const handleEdit = (field : any, currentValue: any) => {
     setEditingField(field);
-    setTempValue(value);
+    setTempValue(currentValue);
   };
 
+  // Función para guardar la edición de un campo
   const handleSave = (field: any) => {
     setProfile({ ...profile, [field]: tempValue });
     setEditingField(null);
   };
 
-  const handleUpdateProfile = () => {
-    // Aquí iría la lógica para enviar la información actualizada al backend
-    Alert.alert("Perfil Actualizado", "Los cambios en el perfil han sido guardados.");
-  };
-
-  const openCertificate = () => {
-    Linking.openURL(profile.certificate);
-  };
-
-  const renderEditView = (field: string, value: string) => (
+  // Función para renderizar la vista de edición
+  const renderEditView = (field: any, value: any) => (
     <TextInput
       style={styles.input}
       value={tempValue}
       onChangeText={setTempValue}
       autoFocus
-      onBlur={() => handleSave(field)}
+      onEndEditing={() => handleSave(field)}
     />
   );
 
-  const renderTextView = (field, value) => (
-    <TouchableOpacity onPress={() => handleEdit(field, value)}>
+  // Función para renderizar la vista de texto
+  const CustomTextView = ({ field, value }: { field: any, value: any }) => (
+    <TouchableOpacity style={{ width: 200 }} onPress={() => handleEdit(field, value)}>
       <Text style={styles.textValue}>{value}</Text>
     </TouchableOpacity>
   );
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.profileSection}>
-        <Image
-          source={{ uri: profile.picture }}
-          style={styles.profilePic}
-        />
-        <Text style={styles.label}>Name:</Text>
-        {editingField === "name" ? renderEditView("name", profile.name) : renderTextView("name", profile.name)}
-        <Text style={styles.label}>Email:</Text>
-        {editingField === "email" ? renderEditView("email", profile.email) : renderTextView("email", profile.email)}
-        <Text style={styles.label}>Phone Number:</Text>
-        {editingField === "phoneNumber" ? renderEditView("phoneNumber", profile.phoneNumber) : renderTextView("phoneNumber", profile.phoneNumber)}
-        <Text style={styles.label}>Address:</Text>
-        {editingField === "address" ? renderEditView("address", profile.address) : renderTextView("address", profile.address)}
-        <Text style={styles.label}>Description:</Text>
-        {editingField === "description" ? renderEditView("description", profile.description) : renderTextView("description", profile.description)}
-        <Text style={styles.label}>Birth Date:</Text>
-        <TouchableOpacity>
-          <Text style={styles.textValue}>{profile.birthDate}</Text>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.profileContainer}>
+          <Image style={styles.profileImage} source={{ uri: profile.picture }} />
+          <Text style={styles.nameText}>{profile.name}</Text>
+
+          <View style={styles.ratingContainer}>
+            <Icon name="star" size={18} color="#000" style={styles.iconStyle} />
+            {/*<Text style={styles.ratingText}>{profile.averageRating}</Text>*/}
+          </View>
+        </View>
+
+        <View style={styles.detailsContainer}>
+          <Icon name="envelope" size={18} color="#000" style={styles.iconStyle} />
+          {editingField === "email" ? renderEditView("email", profile.email) :
+            <CustomTextView field={"email"} value={profile.email} />}
+        </View>
+        <View style={styles.detailsContainer}>
+          <Icon name="phone" size={18} color="#000" style={styles.iconStyle} />
+          {editingField === "phoneNumber" ? renderEditView("phoneNumber", profile.phoneNumber) :
+            <CustomTextView field={"phoneNumber"} value={profile.phoneNumber} />}
+        </View>
+        <View style={styles.detailsContainer}>
+          <Icon name="map-marker-alt" size={18} color="#000" style={styles.iconStyle} />
+          <CustomTextView field={"address"} value={profile.address} />
+        </View>
+        <View style={styles.detailsContainer}>
+          <Icon name="birthday-cake" size={18} color="#000" style={styles.iconStyle} />
+          <CustomTextView field={"birthDate"} value={profile.birthDate} />
+        </View>
+        <View style={styles.detailsContainer}>
+          <Icon name="user-tie" size={18} color="#000" style={styles.iconStyle} />
+          <CustomTextView field={"profession"} value={profile.profession.professionName} />
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => {
+        }}>
+          <Text style={styles.buttonText}>Update Profile</Text>
         </TouchableOpacity>
-        <Text style={styles.label}>Professión:</Text>
-        <TouchableOpacity>
-          <Text style={styles.textValue}>{profile.profession}</Text>
+
+        <TouchableOpacity style={styles.button} onPress={signOut}>
+          <Text style={styles.buttonText}>Log Out</Text>
         </TouchableOpacity>
-        <Text style={styles.label}>Certificate:</Text>
-        <TouchableOpacity onPress={openCertificate}>
-          <Text style={styles.linkText}>Show Certificate</Text>
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity style={styles.updateButton} onPress={handleUpdateProfile}>
-        <Text style={styles.updateButtonText}>Update</Text>
-      </TouchableOpacity>
-      <View style={styles.bottomSection}>
-        <TouchableOpacity style={styles.logoutButton} onPress={signOut}>
-          <Icon name="sign-out" size={20} color="#fff" />
-          <Text style={styles.logoutButtonText}>Log Out</Text>
-        </TouchableOpacity>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f7f7f7"
+    backgroundColor: "#fff"
   },
-  profileSection: {
-    padding: 20,
+  scrollViewContent: {
     alignItems: "center",
-    borderBottomWidth: 1,
-    borderBottomColor: "#e1e1e1"
+    paddingVertical: 20,
+    flex: 1
   },
-  profilePic: {
+  profileContainer: {
+    alignItems: "center",
+    marginBottom: 20
+  },
+  profileImage: {
     width: 150,
     height: 150,
     borderRadius: 75,
+    borderWidth: 4,
+    borderColor: "#d3d3d3",
     marginBottom: 10
   },
-  label: {
-    alignSelf: "flex-start",
+  nameText: {
+    fontSize: 24,
+    fontWeight: "600",
+    marginBottom: 5
+  },
+  ratingText: {
     fontSize: 16,
-    fontWeight: "bold",
-    marginTop: 10
+    color: "#808080",
+    marginBottom: 15
+  },
+  detailsContainer: {
+    width: "100%",
+    justifyContent: "center",
+    gap: 20,
+    flexDirection: "row",
+    marginLeft: 60
+
+  },
+  iconStyle: {
+    width: 25,
+    height: 50,
+    textAlign: "left",
+    paddingVertical: 10
   },
   textValue: {
-    alignSelf: "flex-start",
     fontSize: 16,
-    color: "#333",
-    marginBottom: 5
-  },
-  linkText: {
-    alignSelf: "flex-start",
-    fontSize: 16,
-    color: "#0000FF",
-    textDecorationLine: "underline",
-    marginBottom: 5
+    color: "#000",
+    paddingVertical: 10
   },
   input: {
-    alignSelf: "stretch",
     fontSize: 16,
+    color: "#000",
+    paddingVertical: 10,
     borderBottomWidth: 1,
     borderColor: "gray",
-    paddingVertical: 2,
-    marginBottom: 5
+    width: "100%"
   },
-  updateButton: {
+  button: {
     backgroundColor: "#6200ee",
-    padding: 10,
-    borderRadius: 5,
+    padding: 15,
+    borderRadius: 8,
+    marginVertical: 10,
+    width: "80%"
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "600"
+  },
+  ratingContainer: {
     alignItems: "center",
-    justifyContent: "center",
-    marginHorizontal: 20,
-    marginTop: 10,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 2,
-    elevation: 4
-  },
-  updateButtonText: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight: "bold"
-  },
-  bottomSection: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    padding: 10
-  },
-  logoutButton: {
-    backgroundColor: "#d9534f",
-    padding: 10,
-    borderRadius: 5,
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  logoutButtonText: {
-    color: "#fff",
-    marginLeft: 5,
-    fontWeight: "bold",
-    fontSize: 18
+    gap: 2,
+    flexDirection: "row"
+
   }
 });
+
+export default ProfileScreen;
